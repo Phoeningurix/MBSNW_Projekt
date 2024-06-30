94fe5fdff200072cac04c800a7f501a878899ade
Phoeningurix <159692676+Phoeningurix@users.noreply.github.com>
2024-06-30 22:52:04 +0200

    added Zielorte

--- app/src/main/java/de/htw/mbsnw_projekt/database/AppDatabase.java
+++ app/src/main/java/de/htw/mbsnw_projekt/database/AppDatabase.java
@@ -51,8 +51,35 @@ public abstract class AppDatabase extends RoomDatabase {
         ortListeDao.insert(new OrtListe(false, "Default"));
         zielortDao.insert(new Zielort(52.533579, 13.417889, "Wasserturm", 1));
         zielortDao.insert(new Zielort(52.455100, 13.524743, "HTW Ufer", 1));
-        zielortDao.insert(new Zielort(52.545587, 13.423519, "Käthe-Kollwitz-Gymnasium", 1));
-        zielortDao.insert(new Zielort(52.543908, 13.424302, "Falafel Aladdin (Prenzlauer Berg)", 1));
+        //zielortDao.insert(new Zielort(52.545587, 13.423519, "Käthe-Kollwitz-Gymnasium", 1));
+        //zielortDao.insert(new Zielort(52.543908, 13.424302, "Falafel Aladdin (Prenzlauer Berg)", 1));
+        zielortDao.insert(new Zielort(52.538491, 13.413205, "Kulturbrauerei", 1));
+        zielortDao.insert(new Zielort(52.521214, 13.413397, "Weltzeituhr", 1));
+        zielortDao.insert(new Zielort(52.519782, 13.407215, "Neptunbrunnen", 1));
+        zielortDao.insert(new Zielort(52.518899, 13.400410, "Berliner Dom", 1));
+        zielortDao.insert(new Zielort(52.516455, 13.393937, "Bebelplatz", 1));
+        zielortDao.insert(new Zielort(52.516294, 13.377942, "Brandenburger Tor", 1));
+        zielortDao.insert(new Zielort(52.514564, 13.350604, "Siegessäule", 1));
+        zielortDao.insert(new Zielort(52.514379, 13.392867, "Französischer Dom", 1));
+        zielortDao.insert(new Zielort(52.516799, 13.406851, "Nikolaikirche", 1));
+        zielortDao.insert(new Zielort(52.527983, 13.426485, "Märchenbrunnen", 1));
+        zielortDao.insert(new Zielort(52.528001, 13.437026, "Friedrichshain", 1));
+        zielortDao.insert(new Zielort(52.502635, 13.446189, "Oberbaumbrücke", 1));
+        zielortDao.insert(new Zielort(52.542438, 13.403157, "Mauerpark", 1));
+        zielortDao.insert(new Zielort(52.520247, 13.372694, "Bundestag", 1));
+        zielortDao.insert(new Zielort(52.523649, 13.369821, "Hauptbahnhof", 1));
+        zielortDao.insert(new Zielort(52.476208, 13.416417, "Tempelhofer Feld", 1));
+        zielortDao.insert(new Zielort(52.497633, 13.371389, "Park am Gleisdreieck", 1));
+        zielortDao.insert(new Zielort(52.504613, 13.335800, "Ku'Damm", 1));
+        zielortDao.insert(new Zielort(52.504613, 13.335800, "Ku'Damm", 1));
+        zielortDao.insert(new Zielort(52.504829, 13.279248, "Funkturm", 1));
+        zielortDao.insert(new Zielort(52.421194, 13.175243, "Wannsee", 1));
+        zielortDao.insert(new Zielort(52.520038, 13.295686, "Schloss Charlottenburg", 1));
+        zielortDao.insert(new Zielort(52.539225, 13.212568, "Zitadelle Spandau", 1));
+        zielortDao.insert(new Zielort(52.444206, 13.573872, "Schloss Köpenik", 1));
+        zielortDao.insert(new Zielort(52.553037, 13.465142, "Weißer See", 1));
+        zielortDao.insert(new Zielort(52.575748, 13.407418, "Schlosspark Pankow", 1));
+        zielortDao.insert(new Zielort(52.518542, 13.388624, "Friedrichstraße", 1));
     }
 
     private static void doInBackground(Runnable r) {
